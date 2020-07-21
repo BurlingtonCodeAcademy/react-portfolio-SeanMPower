33fdf7388804dfd1a8d2d4094fe75474eb9f0a4a
SeanPower <seanmp22@gmail.com>
2020-07-20 23:44:36 -0400

    Fiddling with color scheme

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
     return(
         <div className='main' id='home'>
-            <div><h1>Hi, I'm Sean,</h1></div>
+            <div><h1>Hi, I'm <span id='S'>S</span>ean,</h1></div>
             <div><h1>Web Developer.</h1></div>
             <div id='largeS'>S</div>
         </div>
