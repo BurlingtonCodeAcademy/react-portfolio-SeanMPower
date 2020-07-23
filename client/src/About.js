aab8a129398f2da7dc2bedd9e962e4954ebc2efb
SeanPower <seanmp22@gmail.com>
2020-07-22 23:01:52 -0400

    Added to Projects and About

--- client/src/About.js
+++ client/src/About.js
@@ -7,12 +7,22 @@ function About(props) {
     <div>
       {modal ? (
         <div className="main" id="aboutPage">
-            <div id="main-col1">
-                <h1><span id="S">A</span>bout <span id="S">M</span>e</h1>
-                <h5>I'm an aspiring web developer living in South Burlington, VT. </h5>
+          <div id="main-col1">
+            <div id="about-top-left">
+              <h1>
+                <span id="S">A</span>bout <span id="S">M</span>e
+              </h1>
+              <h6>
+                Hello there! My name is Sean Power. I'm an aspiring web developer living in South Burlington, VT. I have a passion for technology and learning which led me to pursue a career in web development. The idea that there is always more to learn and technologies always changing excites me as it provides a constant challenge and growth opportunities!
+              </h6>
             </div>
-          <div id="profile-pic main-col2">
-            <img src="/img/circle-cropped.png" style={{width: "500px"}} id="profile-pic" />
+          </div>
+          <div id="main-col2">
+            <img
+              src="/img/circle-cropped.png"
+              style={{ width: "500px" }}
+              id="profile-pic"
+            />
           </div>
         </div>
       ) : null}
--- client/src/Projects.js
+++ client/src/Projects.js
@@ -8,7 +8,14 @@ function Projects(props) {
       {modal ? (
       <div className="main" id="projectsPage">
           <div id='project-header'><h1><span id="S">P</span>roject <span id="S">P</span>ortfolio</h1></div>
+          <div className="project-thumb"><h2>Tic-Tac-Toe</h2><br /><img src="/img/tic-tac-thumb.png" style={{width: '200px', height: '200px' }} /><br></br><br />A collaborative effort with Christian Dow. We created a functional tic-tac-toe game that can be played with two players or one player vs. CPU. It taught us about DOM manipulation, working with timers, and lots of work with arrays.</div>
+          <div className="project-thumb"><h2>Yelpington</h2><br /><img src="/img/yelpington-thumb.png" style={{width: '200px', height: '200px'}} /><br /><br />A simple mock-up of a Yelp-inspired site for local Burlington restaurants that pulls in lat/long data from an API and places markers using a map API. In this case, MapBox.</div>
+          <div className="project-thumb"> </div>
+          <div className="project-thumb"> </div>
+          <div className="project-thumb"> </div>
+          <div className="project-thumb"> </div>
       </div>
+      
       ) : null}
       </div>
   )
